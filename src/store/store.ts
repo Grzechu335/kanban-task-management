57b368920c27cb9b32bf0cd9d218d76c8263211b
Grzegorz <grzegorz.skrabucha@gmail.com>
2023-05-05 16:55:39 +0200

    Added persisted state

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,33 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore, createStore } from '@reduxjs/toolkit'
 import DataSliceReducer from './DataSlice'
 import EditModesSliceReducer from './EditModesSlice'
 import UISliceReducer from './UISlice'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
-const store = configureStore({
-    reducer: {
-        data: DataSliceReducer,
-        ui: UISliceReducer,
-        editModes: EditModesSliceReducer,
-    },
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['data'],
+}
+
+const rootReducer = combineReducers({
+    data: DataSliceReducer,
+    ui: UISliceReducer,
+    editModes: EditModesSliceReducer,
 })
 
-export default store
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(persistedReducer)
+
+export const persistor = persistStore(store)
+
+// const store = configureStore({
+//     reducer: {
+
+//     },
+// })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
